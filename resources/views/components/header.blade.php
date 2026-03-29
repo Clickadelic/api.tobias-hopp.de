70bafb48898e586f4a104845aca496994859dde7
Clickadelic <toby.hopp@gmail.com>
2026-03-29 07:22:23 +0200

    some colors

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,18 @@
-<div class="w-full bg-gray-200 backdrop backdrop-blur shadow-md">
-    <header class="container mx-auto flex items-center justify-between py-4">
+<div class="bg-gray-900 shadow-lg backdrop-blur border-primary border-b-2 w-full backdrop">
+    <header class="flex justify-between items-center mx-auto py-4 container">
         <h1 className="app-logo">
-            <a href="/" class="flex flex-row justify-between gap-1 sm:gap-2 text-primary group">
-                <span class="font-la-belle-aurore text-2xl">Toby's</span>
-                <span class="text-xl font-medium leading-snug inline-block text-gray-900">
+            <a href="/" class="group flex flex-row justify-between gap-1 sm:gap-2">
+                <span class="font-la-belle-aurore text-primary text-2xl">Toby's</span>
+                <span class="inline-block font-medium text-gray-300 text-xl leading-snug">
                     <span class="text-primary">{</span> API-Service <span class="text-primary">}</span>
                 </span>
             </a>
         </h1>
         <nav>
-            <ul class="flex gap-3">
-                <li><a href="/" class="font-medium" title="Start">Start</a></li>
-                <li><a href="/about" class="font-medium" title="About">About</a></li>
-                <li><a href="/docs" class="font-medium" title="Docs">Docs</a></li>
+            <ul class="flex gap-6">
+                <li><a href="/" class="font-medium text-gray-300" title="Start">Start</a></li>
+                <li><a href="/about" class="font-medium text-gray-300" title="About">About</a></li>
+                <li><a href="/docs" class="font-medium text-gray-300" title="Docs">Docs</a></li>
             </ul>
         </nav>
     </header>
