a16bb28f712b51794af191777a91eb00c6151ab3
Clickadelic <toby.hopp@gmail.com>
2026-03-29 07:22:23 +0200

    some colors

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
-<div class="w-full py-3 bg-gray-200 backdrop backdrop-blur">
-    <footer class="container mx-auto">
-        <p class="text-gray-800 text-center text-sm">Made with <img src="./images/heart-outline.svg" width="16" height="16" class="mx-1 -mt-px inline-flex" alt="Heart" /> by <a href="https://www.tobias-hopp.de/de" target="_blank" class="hover:underline">Tobias Hopp</a>.</p>
+<div class="bg-gray-900 backdrop-blur py-3 border-primary border-t-2 w-full backdrop">
+    <footer class="mx-auto container">
+        <p class="text-gray-300 text-sm text-center">Made with <img src="./images/heart-outline.svg" width="16" height="16" class="inline-flex mx-1 -mt-px" alt="Heart" /> by <a href="https://www.tobias-hopp.de/de" target="_blank" class="hover:underline">Tobias Hopp</a>.</p>
     </footer>
 </div>
