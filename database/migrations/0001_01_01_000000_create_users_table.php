507c66726cbd96471683e0a6aa28231f5f448f63
Clickadelic <toby.hopp@gmail.com>
2026-03-30 02:28:32 +0200

    Seeders Update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken(); // Kann eventuell entfernt werden, wenn nicht benötigt (nur bei Web-Authentifizierung relevant)
             $table->timestamps();
         });
 
