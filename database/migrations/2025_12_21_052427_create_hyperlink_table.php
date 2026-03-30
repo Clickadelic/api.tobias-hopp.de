971a7f65bb23071a734f8cf823ae3d39f5f30c6b
Clickadelic <toby.hopp@gmail.com>
2026-03-30 02:37:03 +0200

    Seeder fix, Tablename correction

--- database/migrations/2025_12_21_052427_create_hyperlink_table.php
+++ database/migrations/2025_12_21_052427_create_hyperlink_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('hyperlink', function (Blueprint $table) {
+        Schema::create('hyperlinks', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
             $table->string('description');
