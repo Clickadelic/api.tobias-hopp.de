2ed85a031a557035e6b531bcf03bc09185bd4ad5
Clickadelic <toby.hopp@gmail.com>
2026-03-30 02:40:53 +0200

    Todo added

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Todo: remember_token kann entfernt werden, wenn nicht benötigt (nur bei Web-Authentifizierung relevant)
 return new class extends Migration
 {
     /**
