9209e60554a8f14168ca5f15cfbc348d2134562d
Clickadelic <toby.hopp@gmail.com>
2026-05-28 23:41:20 +0200

    Gradient on Frontpage

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -4,9 +4,10 @@
 
         <div class="max-w-lg w-full border-2 border-primary rounded m-4">
             <div class="flex flex-col justify-center items-center bg-gray-900 p-4 md:py-12 rounded">
-                <h2 class="flex justify-center items-center gap-2 mb-3 font-medium text-gray-300 text-sm md:text-2xl"><span
+                <h2 class="flex justify-center items-center gap-2 mb-3 font-medium  text-sm md:text-2xl bg-linear-to-r from-teal-400 to-yellow-200 bg-clip-text text-transparent"><span
                         class="font-la-belle-aurore text-primary text-2xl">Toby's</span><span class="text-primary">{
                     </span><span class="text-wrap">API-Service</span><span class="text-primary">}</span></h2>
+
                 <p class="text-gray-300 text-sm text-center">This API provides access &shy;to various <br>features and
                     functionalities &shy;of Toby's API-platform.</p>
                 <div class="flex justify-center items-center gap-4 text-center">
