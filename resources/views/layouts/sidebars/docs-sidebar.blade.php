5428b7c6e986b84dc33fa3fa581cca65d1fb5e80
Clickadelic <toby.hopp@gmail.com>
2026-05-30 21:56:59 +0200

    terms

--- resources/views/layouts/sidebars/docs-sidebar.blade.php
+++ resources/views/layouts/sidebars/docs-sidebar.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<h2 class="font-medium text-2xl text-gray-200">Navigation</h2>
+<ul class="list-inside text-gray-300">
+	<li><a href="#" target="_self" class="flex gap-2 hover:text-white hover:underline underline-offset-5"><x-heroicon-o-chevron-right class="size-4 mt-1" />Link</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,12 @@ Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/docs', [PageController::class, 'docs'])->name('docs');
 
-Route::get('/cookie-policy', [PageController::class, 'cookiepolicy'])->name('cookie-policy');
 Route::get('/disclaimer', [PageController::class, 'disclaimer'])->name('disclaimer');
+Route::get('/cookie-policy', [PageController::class, 'cookiepolicy'])->name('cookie-policy');
 
 Route::get('/terms-of-service', [PageController::class, 'termsofservice'])->name('terms-of-service');
 Route::get('/terms-of-use', [PageController::class, 'termsofuse'])->name('terms-of-use');
+Route::get('/terms-of-privacy', [PageController::class, 'termsofprivacy'])->name('terms-of-privacy');
 
 // if (config('app.env') === 'local') {
 	include_once __DIR__ . '/dev.php';
