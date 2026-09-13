106efb3c82e48b50d3de9138ae225448faa551c7
Clickadelic <toby.hopp@gmail.com>
2026-09-13 18:15:41 +0200

    Permissions in models

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseModel extends Model
@@ -12,4 +13,9 @@ abstract class BaseModel extends Model
 	public $incrementing = false;
 
 	protected $keyType = 'string';
+
+	public function newCollection(array $models = []): Collection
+	{
+		return new Collection($models);
+	}
 }
